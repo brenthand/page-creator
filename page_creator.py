f4e4b7ed79525403c40dec21c082971bf70585a1
brent <hand.brent@gmail.com>
2015-10-16 01:07:32 -0400

    added argument parsing and the ability to use templates

--- page_creator.py
+++ page_creator.py
@@ -1,16 +1,31 @@
 import argparse
 
 
-start_html = "<html><body>"
-end_html = "</body></html>"
 
-def create_links(num_pages, page_name):
+
+def get_html_template(header_path, footer_path):
+    start_html = "<html></body>"
+    end_html = "</body></html>"
+
+    if(header_path != ""):
+        with open(header_path) as f:
+            start_html = f.read()
+
+    if(footer_path != ""):
+        with open(footer_path) as f:
+            end_html = f.read()
+
+    return start_html, end_html    
+
+
+
+def create_links(num_pages, page_name, header, footer):
     #count = 0
     links = ""
     
     for count in  range(num_pages):
         links += "<a href='" + page_name + str(count) + ".html'>" + page_name + str(count) + "</a><br />"
-
+    start_html, end_html = get_html_template(header, footer)
     html = start_html + links + end_html
     
     for count in  range(num_pages):
@@ -22,11 +37,13 @@ def create_links(num_pages, page_name):
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('number', default=5, type=int, help="Number of pages to create")
-    parser.add_argument('filename', default="test", help="Base filename")
+    parser.add_argument('-n', dest="number", default=5, type=int, help="Number of pages to create")
+    parser.add_argument('-f', dest="filename", default="test", help="Base filename")
+    parser.add_argument('-b', dest="header", default="", required=False, help="Header template path")
+    parser.add_argument('-e', dest="footer", default="", required=False, help="Footer template path")
     args = parser.parse_args()
     
-    create_links(args.number, args.filename)
+    create_links(args.number, args.filename, args.header, args.footer)
 
     
         
